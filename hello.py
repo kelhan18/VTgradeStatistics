728a1570418e0975302b4487ad5e660d47cde686
Keller Han <keller@Kellers-MacBook-Pro.local>
2017-08-02 13:09:08 -0400

    <added social media icons>

--- hello.py
+++ hello.py
@@ -5,42 +5,45 @@ import pandas as pd
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
+def show_home():
+    return render_template('index.html')
+@app.route('/index.html')
 def show_main():
 
     return render_template('index.html')
 
-@app.route('/cs')
+@app.route('/cs.html')
 
 def show_cs():
 
     return render_template('cs.html')
 
-@app.route('/contact')
+@app.route('/contact.html')
 def show_contact():
 
     return render_template('contact.html')
 
-@app.route('/ece')
+@app.route('/ece.html')
 def show_cpe():
-    return render_template('ece.html')
+    return render_template('/ece.html')
 
-@app.route('/aero')
+@app.route('/aero.html')
 def show_aero():
     return render_template('aero.html')
 
-@app.route('/math')
+@app.route('/math.html')
 def show_math():
     return render_template('math.html')
 
-@app.route('/bit')
+@app.route('/bit.html')
 def show_bit():
     return render_template('bit.html')
 
-@app.route('/finance')
+@app.route('/finance.html')
 def show_finance():
     return render_template('finance.html')
 
-@app.route('/accounting')
+@app.route('/acis.html')
 def show_accounting():
     return render_template('acis.html')
 
